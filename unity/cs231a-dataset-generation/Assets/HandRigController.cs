5a9be596baa439332142fb046b5b3a37d14aa41f
Benjamin Wiberg <benjaminwiberg@gmail.com>
2017-05-26 21:00:49 -0700

    Create hand rig scene

--- unity/cs231a-dataset-generation/Assets/HandRigController.cs
+++ unity/cs231a-dataset-generation/Assets/HandRigController.cs
@@ -16,25 +16,62 @@ public class HandRigController : MonoBehaviour
 	}
 
 	[System.Serializable]
-	public class Palm
+	public class Finger
 	{
-		[System.Serializable]
-		public class Thumb
-		{
-			public JointAngle Palm;
-			public JointAngle Joint1;
-			public JointAngle Joint2;
-		}
-
-		public Thumb thumb;
+		public JointAngle Palm;
+		public JointAngle Joint1;
+		public JointAngle Joint2;
+		public JointAngle Joint3;
 	}
 
-	public Palm palm;
+	[System.Serializable]
+	public class Thumb
+	{
+		public JointAngle Palm;
+		public JointAngle Joint1;
+		public JointAngle Joint2;
+	}
+
+	[System.Serializable]
+	public class Hand
+	{
+		public JointAngle root;
+		public Thumb thumb;
+		public Finger index;
+		public Finger middle;
+		public Finger ring;
+		public Finger pinky;
+	}
+
+	public Hand hand;
 
 	private JointAngle[] joints;
 
 	private void Start ()
 	{
+		joints = new JointAngle[]{
+			hand.root,
+			hand.thumb.Palm,
+			hand.thumb.Joint1,
+			hand.thumb.Joint2,
+			hand.index.Palm,
+			hand.index.Joint1,
+			hand.index.Joint2,
+			hand.index.Joint3,
+			hand.middle.Palm,
+			hand.middle.Joint1,
+			hand.middle.Joint2,
+			hand.middle.Joint3,
+			hand.ring.Palm,
+			hand.ring.Joint1,
+			hand.ring.Joint2,
+			hand.ring.Joint3,
+			hand.pinky.Palm,
+			hand.pinky.Joint1,
+			hand.pinky.Joint2,
+			hand.pinky.Joint3,
+		};
+
 		Debug.Log("Saving initial angles of joints.");
 		ForEachJointAngle((JointAngle joint) => {
 			joint.InitialAngles = joint.Transform.localEulerAngles;
@@ -51,11 +88,7 @@ public class HandRigController : MonoBehaviour
 	}
 
 	private void ForEachJointAngle(Action<JointAngle> lambda) {
-		foreach (var joint in new JointAngle[]{
-			palm.thumb.Palm,
-			palm.thumb.Joint1,
-			palm.thumb.Joint2 
-		})
+		foreach (var joint in joints)
 		{
 			lambda(joint);
 		}
